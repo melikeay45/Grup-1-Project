dd4e30ff1e2958d2ffb1ae347b3ba0f1eef9ee52
Melike Aydın <melike.ay45@gmail.com>
2024-05-27 15:05:22 +0300

    [Features]: Add common file and EntityBase class.
    The EntityBase class serves as a base class for all entities within the project. It provides common properties that are useful for tracking the state and lifecycle of entities.

---
+++ App.Domain/Common/EntityBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Common
+{
+    public class EntityBase
+    {
+        public Guid Id { get; set; }
+
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+    }
+}
